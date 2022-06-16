50b6b269581c0ab3c6a93ee1d4c97690e5352a10
Neto <jbsn2019@gmail.com>
2022-06-15 22:05:36 -0300

    Creating the login page

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { ISession } from 'src/interface/session';
+import { set } from 'src/store/auth/session.actions';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  form = new FormGroup({
+    login: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)])
+  })
+
+  constructor(
+    private store: Store<ISession>,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  async onSubmit () {
+    this.store.dispatch(set({isAuthenticated: true}));
+    this.router.navigate(['/']);
+  }
+
+}
---
+++ src/app/pages/login/login.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginRoutingModule } from './login-routing.module';
+import { LoginComponent } from './login.component';
+
+//Material components
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    LoginComponent
+  ],
+  imports: [
+    CommonModule,
+    LoginRoutingModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatGridListModule,
+    MatButtonModule,
+    ReactiveFormsModule
+  ]
+})
+export class LoginModule { }
