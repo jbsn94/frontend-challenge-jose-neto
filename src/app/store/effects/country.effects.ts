48868033862835bdd3db99056dfb2a876153148f
Neto <jbsn2019@gmail.com>
2022-06-27 21:21:17 -0300

    Removing unused code.

--- src/app/store/effects/country.effects.ts
+++ src/app/store/effects/country.effects.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
-import { catchError, EMPTY, map, mergeMap, of } from "rxjs";
+import { catchError, map, mergeMap, of } from "rxjs";
 import { CountryService } from "src/app/service/country/country.service";
 import { CountryActions } from '../actions';
 
